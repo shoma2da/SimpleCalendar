b6f4f16f700ec259edec4715350e6277425422ac
shoma2da <marcos.shoichi.matsuda@gmail.com>
2013-09-07 23:52:43 +0900

    テストケースを追加

--- android/test/src/com/tech_tec/android/simplecalendar/test/CellAdapterTest.java
+++ android/test/src/com/tech_tec/android/simplecalendar/test/CellAdapterTest.java
@@ -1,10 +1,12 @@
 package com.tech_tec.android.simplecalendar.test;
 
-import com.tech_tec.android.simplecalendar.view.CellAdapter;
-
 import android.content.Context;
 import android.test.AndroidTestCase;
+import android.view.View;
 import android.widget.GridView;
+
+import com.tech_tec.android.simplecalendar.view.CellAdapter;
+import com.tech_tec.android.simplecalendar.view.HeaderView;
 
 public class CellAdapterTest extends AndroidTestCase {
     
@@ -15,4 +17,13 @@ public class CellAdapterTest extends AndroidTestCase {
         assertEquals(CellAdapter.COLUMN_NUMBER, adapter.getCount());
     }
     
+    public void testはじめの規定個数は曜日が取得できる() {
+        Context context = getContext();
+        CellAdapter adapter = new CellAdapter(context, new GridView(context));
+        
+        for (int i = 0; i < CellAdapter.COLUMN_NUMBER; i++) {
+            View view = adapter.getView(i, null, null);
+            assertEquals(HeaderView.class, view.getClass());
+        }
+    }
 }
