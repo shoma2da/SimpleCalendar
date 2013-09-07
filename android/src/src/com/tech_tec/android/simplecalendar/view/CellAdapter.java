24dc78df9ee97136fb7597ebd1025791c239c550
shoma2da <marcos.shoichi.matsuda@gmail.com>
2013-09-07 22:36:28 +0900

    曜日を仮表示

--- android/src/src/com/tech_tec/android/simplecalendar/view/CellAdapter.java
+++ android/src/src/com/tech_tec/android/simplecalendar/view/CellAdapter.java
@@ -13,6 +13,7 @@ import android.widget.TextView;
 
 public class CellAdapter extends ArrayAdapter<String> {
     
+    public static int COLUMN_NUMBER = 7;
     public static int ROW_NUMBER = 6;
     
     private LayoutInflater mInflater;
@@ -22,14 +23,51 @@ public class CellAdapter extends ArrayAdapter<String> {
         super(context, 0);
         mInflater = LayoutInflater.from(context);
         mGridView = gridView;
+        
+        for (int i = 0; i < COLUMN_NUMBER; i++) {
+            add(null);
+        }
     }
     
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
+        if (position < COLUMN_NUMBER) {
+            View view = mInflater.inflate(R.layout.layout_header, null);
+            TextView textView = (TextView)view.findViewById(R.id.text);
+            
+            switch (position) {
+            case 0:
+                textView.setText("月");
+                break;
+            case 1:
+                textView.setText("火");
+                break;
+            case 2:
+                textView.setText("水");
+                break;
+            case 3:
+                textView.setText("木");
+                break;
+            case 4:
+                textView.setText("金");
+                break;
+            case 5:
+                textView.setText("土");
+                break;
+            case 6:
+                textView.setText("日");
+                break;
+            default:
+                break;
+            }
+            
+            return view;
+        }
+        
         View view = mInflater.inflate(R.layout.layout_cell, null);
         
         AbsListView.LayoutParams params = new AbsListView.LayoutParams(
-                android.view.ViewGroup.LayoutParams.MATCH_PARENT,
+                mGridView.getWidth() / COLUMN_NUMBER,
                 mGridView.getHeight() / ROW_NUMBER);
         view.setLayoutParams(params);
         
