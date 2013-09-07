22fba0a61f4139284c3febe098b8f9503fcc1db5
shoma2da <marcos.shoichi.matsuda@gmail.com>
2013-09-07 12:03:57 +0900

    カレンダー表示を仮実装

--- android/src/src/com/example/src/MainActivity.java
+++ android/src/src/com/example/src/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.src;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.GridView;
 
 public class MainActivity extends Activity {
 
@@ -9,6 +11,14 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        GridView gridView = (GridView)findViewById(R.id.calendar);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1);
+        gridView.setAdapter(adapter);
+        
+        for (int i = 0; i < 31; i++) {
+            adapter.add(i + 1 + "");
+        }
     }
 
 }
