d9cd62a642cf587399c04fd4e150534f1c120f70
shoma2da <marcos.shoichi.matsuda@gmail.com>
2013-09-08 09:41:43 +0900

    テストケースを追加

--- android/test/src/com/tech_tec/android/simplecalendar/model/DayTest.java
+++ android/test/src/com/tech_tec/android/simplecalendar/model/DayTest.java
@@ -7,12 +7,15 @@ import android.test.AndroidTestCase;
 public class DayTest extends AndroidTestCase {
     
     public void test渡した日付が正しく取得できるか() {
+        doTest渡した日付が正しく取得できるか(1988, 3, 15);
+        doTest渡した日付が正しく取得できるか(1987, 7, 13);
+        doTest渡した日付が正しく取得できるか(2013, 9, 8);
+    }
+    public void doTest渡した日付が正しく取得できるか(int year, int month, int dayValue) {
         Calendar calendar = Calendar.getInstance();
-        calendar.set(1988, 3, 15);
-        
+        calendar.set(year, month, dayValue);
         Day day = new Day(calendar.getTime());
-        
-        assertEquals(15, day.getDayValue().toInt());
+        assertEquals(dayValue, day.getDayValue().toInt());
     }
     
 }
