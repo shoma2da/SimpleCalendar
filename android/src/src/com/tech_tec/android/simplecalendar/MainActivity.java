6bebe02c4ae437a41346c08bacf0c0292d596cbb
shoma2da <marcos.shoichi.matsuda@gmail.com>
2013-09-11 22:38:39 +0900

    ViewPagerを仮実装

--- android/src/src/com/tech_tec/android/simplecalendar/MainActivity.java
+++ android/src/src/com/tech_tec/android/simplecalendar/MainActivity.java
@@ -1,6 +1,11 @@
 package com.tech_tec.android.simplecalendar;
 
+import com.tech_tec.android.simplecalendar.fragment.calendar.CalendarFragment;
+
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.ActionBarActivity;
 
 public class MainActivity extends ActionBarActivity {
@@ -8,6 +13,20 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        ViewPager pager = (ViewPager)findViewById(R.id.pager_calendar);
+        pager.setAdapter(new FragmentPagerAdapter(getSupportFragmentManager()) {
+            
+            @Override
+            public int getCount() {
+                return 3;
+            }
+            
+            @Override
+            public Fragment getItem(int arg0) {
+                return new CalendarFragment();
+            }
+        });
     }
     
 }
