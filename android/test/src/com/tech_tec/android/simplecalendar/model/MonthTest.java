71cb2e5f73a22aece02c384e18c9e244fd9d2db8
shoma2da <marcos.shoichi.matsuda@gmail.com>
2013-09-14 22:06:09 +0900

    年数・月数のテストを追加

--- android/test/src/com/tech_tec/android/simplecalendar/model/MonthTest.java
+++ android/test/src/com/tech_tec/android/simplecalendar/model/MonthTest.java
@@ -21,17 +21,26 @@ public class MonthTest extends AndroidTestCase {
     
     public void test年数が取得できる() {
         for (int i = 0; i < 200; i++) {
-            //1970年から200年分をテスト
-            doTest年数が取得できる(1970 + i);
+            int year = 1970 + i; //1970年から200年分をテスト
+            
+            Calendar calendar = Calendar.getInstance();
+            calendar.set(Calendar.YEAR, year);
+            Month month = new Month(calendar.getTime());
+            
+            assertEquals(year, month.getYear());
         }
     }
-    private void doTest年数が取得できる(int year) {
-        Calendar calendar = Calendar.getInstance();
-        calendar.set(Calendar.YEAR, year);
-        
-        Month month = new Month(calendar.getTime());
-        
-        assertEquals(year, month.getYear());
+
+    public void test月数が取得できる() {
+        for (int i = 0; i < 12; i++) {
+            int monthValue = 0 + i; //1月から12月をテスト ※JavaAPIでは0〜11
+            
+            Calendar calendar = Calendar.getInstance();
+            calendar.set(Calendar.MONTH, monthValue);
+            Month month = new Month(calendar.getTime());
+            
+            assertEquals(monthValue + 1, month.getMonth());
+        }
     }
     
     public void testその月の日数分のデータが入る() {
