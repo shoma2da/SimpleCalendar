5ed8ec6f2cd24cc1659faab7513304243759e5e9
shoma2da <marcos.shoichi.matsuda@gmail.com>
2013-09-21 23:44:46 +0900

    日付の表示を仮実装

--- android/src/src/com/tech_tec/android/simplecalendar/fragment/calendar/CalendarFragment.java
+++ android/src/src/com/tech_tec/android/simplecalendar/fragment/calendar/CalendarFragment.java
@@ -5,6 +5,10 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.tech_tec.android.simplecalendar.R;
 
@@ -17,6 +21,21 @@ public class CalendarFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_calendar, null);
+        
+        //TODO 仮実装→未テスト
+        TableLayout daysTable = (TableLayout)view.findViewById(R.id.table_days);
+        int weekCount = daysTable.getChildCount();
+        for (int i = 0; i < weekCount; i++) {
+            TableRow weekLayout = (TableRow)daysTable.getChildAt(i);
+            int dayCount = weekLayout.getChildCount();
+            
+            for (int j = 0; j < dayCount; j++) {
+                TextView daysText = (TextView)weekLayout.getChildAt(j);
+                daysText.setText("" + (i * 7 + j + 1));
+            }
+        }
+        //------------
+        
         return view;
     }
     
