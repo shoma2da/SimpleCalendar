951e86ca45b7c3f36576ccf23baa017c8edeabc3
shoma2da <marcos.shoichi.matsuda@gmail.com>
2013-10-12 11:56:54 +0900

    Analytics SDKライブラリファイルを追加
    ライブラリファイルを誤ったため差し替え
    
    インターネットアクセス権限を追加
    
    Analytics用のコードを埋め込み
    
    Analyics用のリソースファイルを追加

--- android/src/src/com/tech_tec/android/simplecalendar/activity/main/MainActivity.java
+++ android/src/src/com/tech_tec/android/simplecalendar/activity/main/MainActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.ActionBarActivity;
 
+import com.google.analytics.tracking.android.EasyTracker;
 import com.tech_tec.android.simplecalendar.R;
 
 public class MainActivity extends ActionBarActivity {
@@ -24,6 +25,18 @@ public class MainActivity extends ActionBarActivity {
     protected void onResume() {
         super.onResume();
         mViewPager.setCurrentItem(mViewPager.getAdapter().getCount() / 2);
+    }
+    
+    @Override
+    protected void onStart() {
+        super.onStart();
+        EasyTracker.getInstance(this).activityStart(this);
+    }
+    
+    @Override
+    protected void onStop() {
+        super.onStop();
+        EasyTracker.getInstance(this).activityStop(this);
     }
 
 }
